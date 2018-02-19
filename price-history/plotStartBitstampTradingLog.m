4718b01c4ea01b1cfd7b0fd61281749f86d67b30
Landon Mutch <landonmutch@protonmail.com>
2018-02-18 16:04:19 -0800

    organize, update data in folders; format daily-avg-price-plots;

--- price-history/plotStartBitstampTradingLog.m
+++ price-history/plotStartBitstampTradingLog.m
@@ -6,7 +6,7 @@ timeOfBitstamp = 1.316e+09;
 price = avgPrice{:,2};
 time = avgPrice{:,1};
 timeFromTrading(:,1) = time(:,1) - timeOfBitstamp;
-timeExtrapd = linspace(0,1.55e+9 - timeOfBitstamp,40);
+timeExtrapd = linspace(1.52e+09 - timeOfBitstamp, 1.55e+09 - timeOfBitstamp,20);
 
 date = datetime(time,'ConvertFrom','posixtime');
 dateFut = datetime(timeExtrapd + timeOfBitstamp,'ConvertFrom','posixtime');
@@ -22,25 +22,24 @@ figure(2)
 hold on
 grid on
 
-%if logy
-set(gca, 'YScale', 'log');
-%ylim([0.01 1e+5]);
+ax = gca;
+
+if logy
+set(ax, 'YScale', 'log');
 xlim([0 1.55e+9 - timeOfBitstamp]);
 
+else
+ylim([0 25000]);
+ax.YAxis.Exponent = 3;
+end
 
-%else
-%ax = gca;
-%ax.YAxis.Exponent = 3;
-%ylim([0 20000]);
-%xlim([1.45e+9 1.55e+9]);
-%end
+plot(date,price,'LineWidth',2);
+plot(date,tradingExpFun,'r','LineWidth',2);
+plot(dateFut,tradingExpFunFut,'r--','LineWidth',2);
 
-plot(date,price);
-plot(date,tradingExpFun,'r');
-plot(dateFut,tradingExpFunFut,'r--');
-
-title('Figure 3. Daily averaged btcusd trading price, logarithmic scale')
+title('Daily averaged btcusd trading price, logarithmic scale')
 xlabel('Time, year')
 ylabel('Bitcoin price, USD/BTC')
-legend('Daily averaged Bitstamp trades','Constrained exponential fit to 13-Feb-2018');
+legend('Daily averaged Bitstamp trades','Constrained exponential fit to 18-Feb-2018',sprintf('y=*exp(%.3e*x), where x=0 at %.3e Unix timestamp',b, timeOfBitstamp));
+
 end
