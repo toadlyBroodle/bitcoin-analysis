3369015298e16cb592411d3f5f29880996ca4138
Landon Mutch <landonmutch@protonmail.com>
2018-11-04 18:27:28 -0800

    add "functions" for plotting new metrics; update data

--- functions/plotVol_Price.m
+++ functions/plotVol_Price.m
@@ -1,10 +1,10 @@
-function [  ] = plotSP500BTC( sp50013sep2017,btcusdavgprice )
+function [  ] = plotVol_Price( statsvlmout,btcusdavgprice )
 
 SPY = 3.1536e+07; %3.1536e+07 seconds/year
 
 % Prepare stats arrays
-timesp500 = sp50013sep2017{:,1};
-sp500 = sp50013sep2017{:,2};
+timevol = statsvlmout{:,1};
+vol = statsvlmout{:,2};
 %lnsp500 = log(sp500);
 timebtc = btcusdavgprice{:,1};
 btc = btcusdavgprice{:,2};
@@ -15,21 +15,15 @@ maxTime = 1.5463e+09; %1/1/2019
 timeTicks = minTime:SPY:maxTime;
 minPrice = 0;
 maxPriceBtc = 30000;
-maxPriceSp500 = 3000;
-
-% best fit lines
-%fitpolysp500 = fit(timesp500,sp500,'poly1');
-%fitsp500line = fitpolysp500.p1*timeTicks+fitpolysp500.p2;
-%fitpolybtc = fit(timebtc,btc,'poly1');
-%fitbtcline = fitpolybtc.p1*timeTicks+fitpolybtc.p2;
+%maxPriceSp500 = 3000;
 
 % labels
 usd = ' (USD$)';
 btcy = 'USD/BTC';
 btctit = sprintf('Daily %s price',btcy);
-sp500y = 'S&P500';
-sp500tit = sprintf('Daily %s close',sp500y);
-title({sprintf('%s and %s',btctit,sp500tit);...
+voly = 'S&P500';
+voltit = sprintf('Daily %s close',voly);
+title({sprintf('%s and %s',btctit,voltit);...
     '\it\fontsize{10}github.com/toadlyBroodle/bitcoin-analysis/'})
 
 % Plot daily exchange chart
@@ -42,7 +36,7 @@ plot(ax1(1),timebtc,btc,'Color','b')
 
 set(ax1(1),'YColor','b');
 set(ax1(1),'YScale','log')
-set(ax1(1),'ylim', [minPrice,maxPriceBtc])
+%set(ax1(1),'ylim', [minPrice,maxPriceBtc])
 %set(ax1(1),'xlim', [(minTime + SPY),maxTime])
 ax1.XTick = timeTicks;
 xticklabels(2011:1:2019)
@@ -51,14 +45,14 @@ ylabel(ax1(1),sprintf('%s%s',btcy,usd))
 
 yyaxis(ax1(1),'right')
 hold on;
-plot(ax1(1),timesp500,sp500,'Color','r')
+plot(ax1(1),timevol,vol,'Color','r')
 %plot(ax1(1),timeTicks,fitsp500line,'p--','LineWidth',2);
 
-%set(ax1(1),'YScale','log')
-set(ax1(1),'ylim', [minPrice,maxPriceSp500])
+set(ax1(1),'YScale','log')
+%set(ax1(1),'ylim', [minPrice,maxPriceSp500])
 set(ax1(1),'xlim', [minTime,maxTime])
 set(ax1(1),'YColor','r');
-ylabel(ax1(1),sprintf('%s%s',sp500y,usd))
+ylabel(ax1(1),sprintf('%s%s',voly,usd))
 
 % inset linear plot
 ax3 = axes('Position',[.64 .18 .25 .25],...
@@ -67,7 +61,7 @@ ax3 = axes('Position',[.64 .18 .25 .25],...
 
 hold(ax3,'on')
 plot(timebtc,btc,'b');
-plot(timesp500,sp500,'r');
+plot(timevol,vol,'r');
 
 ylabel(ax3,usd);
 yticklabels([0,10000,20000])
